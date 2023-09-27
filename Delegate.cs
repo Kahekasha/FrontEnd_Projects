a646f6a3b929bbde8c0cd3db39fd92d7b6c53fe4
Kahekasha <137182255+Kahekasha@users.noreply.github.com>
2023-09-27 16:22:02 +0530

    Create Delegate.cs

---
+++ Delegate.cs
@@ -0,0 +1,107 @@
+label{
+    font-weight: bolder;
+}
+.badge{
+  padding-right: 30%;
+  font-weight:normal;
+  font-size:medium;
+}
+.form-check{
+  font-weight:lighter;
+}
+.hbtn{
+    color: white;
+    background-color: navy;
+    
+    margin-left: 20px;
+    font-weight: bolder;
+}
+.hbtn:hover{
+    color: darkblue;
+    background-color: white;
+    border-color: darkblue;
+    border-width: 3px solid darkblue;
+}
+.option{
+  font-size:medium;
+  font-weight: normal;
+  margin-right: 10px;
+  padding-left: 5px;
+  margin-bottom: 20px;
+}
+
+/* #sub2{display: none;} */
+#pritable td{border: none;}
+ #table1{
+  /* background-color: aqua; */
+  border: 1px solid black;
+  border-collapse: collapse;
+  width: 50%;
+  height: 300px;
+
+
+} 
+#Tables th,td{
+  
+  border: 1px solid black;
+  padding: 15px;
+
+}
+#CompanyTable th,td{
+  
+  border: 1px solid black;
+  padding: 15px;
+
+}
+#CompanyTable {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.tables td{
+  height: 30px;
+} 
+.tables tr{height: 50px;}
+#table2{
+  /* background-color: lightcoral; */
+  /* width: 25%;
+  height: 300px;
+  position: relative;
+  top: 30px;
+   */
+ 
+}
+#table2 td{width: 50%;
+}
+#Tables{
+  position: relative;
+  margin-bottom: 100px;
+  /* background-color: #7fffd4; */
+  height: auto;
+  display: flex;
+  flex-direction: column; 
+}
+
+#table3{
+
+  /* background-color: lightgreen; */
+  /* position: absolute;
+  left: 30%;
+  top:95% ; */
+
+}
+
+#table3 td{
+  width: 50%;
+
+
+}
+#table4{
+  /* background-color: lightseagreen; */
+  /* position: absolute;
+  right: 7%;
+  bottom: 30%; */
+}
+#usdrs{
+  font-weight:normal;
+}
